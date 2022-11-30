ca11ce7b032b74e7fa9ca09da4a0f11406fa1e64
Christian Casper Hofma <cchofma@live.dk>
2022-11-30 14:33:41 +0100

    First commit

---
+++ src/main/java/com/devopsinc/bugscout/BugscoutApplication.java
@@ -0,0 +1,13 @@
+package com.devopsinc.bugscout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugscoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugscoutApplication.class, args);
+    }
+
+}
